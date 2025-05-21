168f85b8b0ca6c6b8f9f94bed6d2e0af1bb87b93
Rohit Panwar <108470973+RohitPanwar03@users.noreply.github.com>
2025-05-21 18:15:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Router> 
       <Routes>
       
-      <Route path="/" element={</Layout>}>
+      <Route path="/" element={<Layout/>}>
       <Route index element={<HomePage/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
