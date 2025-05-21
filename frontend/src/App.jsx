c040a00033a8d65271bb74c165dc3dab97d6724a
Rohit Panwar <108470973+RohitPanwar03@users.noreply.github.com>
2025-05-21 18:13:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import { lazy,Supspense} from 'react';
+import { lazy} from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from "./Components/Header"
+import Layout from "./Components/Layout"
 const HomePage = lazy(()=>('./Pages/Login'));
 const Login = lazy(()=>('./Pages/Register'));
 const Register = lazy(()=>('./Components/Layout'));
@@ -14,12 +14,10 @@ function App() {
 
   return (
 
-    <Router>
-      <Header/>
-     <Supspense fallback={<h1>Loading...</h1>}>
-    <Routes>
+    <Router> 
+      <Routes>
       
-
+      <Route path="/" element={</Layout}>
       <Route index element={<HomePage/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
@@ -27,10 +25,9 @@ function App() {
       <Route path='/my-posts' element={<MyPosts/>}/>
       <Route path='/post-details/:id' element={<PostDetails/>}/>
       <Route path='/update-post/:id' element={<UpdatePost/>}/>
+      </Route>  
       <Route path='*' element={<h1 className='text-3xl text-center'>404 Not Found</h1>}/>
-     
-    </Routes>
- </Supspense>
+      </Routes>
     </Router>
 
   )
